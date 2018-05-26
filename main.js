8fda8ffb22f5517502419852c09a28d03277dd5a
Preethesh Puthran <puthranpreethesh@gmail.com>
2018-05-26 15:20:30 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const {owner,prefix,token,dev} = require('./config.json');
+const client = new  Discord.Client(); 
+const util =  require('./src/utilities/discordHelpers.js');
+
+console.log(prefix);
+
+client.on('ready', () => {
+    client.user.setGame('Playing with Coconut Meme ( ͡° ͜ʖ ͡°)');
+    console.log(`connected to`);
+});
+
+client.on('message', (message) => {
+    let args = util.parseArgs(message.content);
+    let command = util.getCommand(message.content);
+    switch(command){
+        case 'ping':
+            message.channel.send('Pong');
+            break; 
+        case 'server-info':
+            message.channel.send("hi?");
+            break; 
+        case 'icon':
+            message.channel.send(message.author.avatarURL);
+            break; 
+        case 'yuri':
+            message.reply('',{
+                files: [
+                    './Yuri.png'
+                ]
+            });
+            break;
+        case `why is ${args[3]}'s waifu garbo?`:
+            message.reply('Your waifu is trash');
+            break;
+    }
+});
+
+client.login(token);
---
+++ src/utilities/discordHelpers.js
@@ -0,0 +1,22 @@
+const {owner, prefix} = require('../../config.json')
+
+String.prototype.capitalize = () => {
+    return this.charAt(0).toUpperCase();
+};
+
+String.prototype.isNumber = () => {
+    return !isNaN(parseInt(this));
+};
+
+function parseArgs(str){
+    return str.slice(prefix.length).trim().toLowerCase().split(/ +/g); 
+}
+
+function getCommand(str){
+    return parseArgs(str).shift();   
+}
+
+return module.exports = {
+    parseArgs : parseArgs,
+    getCommand : getCommand
+}
