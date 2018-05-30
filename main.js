91b1fe3d2c99587e3c3ca67c4c74f6d94e8ec874
Preethesh Puthran <puthranpreethesh@gmail.com>
2018-05-30 18:15:48 -0400

    Fixed the server-info command

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ client.on('ready', () => {
     console.log(`connected to ${owner}'s server`);
 });
 
-<<<<<<< HEAD
 // client.on("guildMemberAdd", member => {
 //     const guild = member.guild;
 //     newUsers.set(member.id, member.user);
@@ -26,21 +25,6 @@ client.on('ready', () => {
 // });
 
 client.on('message', message => {
-=======
-client.on("guildMemberAdd", (member) => {
-    const guild = member.guild;
-    newUsers.set(member.id, member.user);
-    
-    if (newUsers.size > 10) {
-      const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
-      const userlist = newUsers.map(u => u.toString()).join(" ");
-      defaultChannel.send("Welcome our new users!\n" + userlist);
-      newUsers.clear();
-    }
-});
-
-client.on('message', (message) => {
->>>>>>> c50e25ed938b7eb985a96170b859337bad6936d6
     if(message.author.bot) return; 
     if(message.content.indexOf(prefix) !== 0) return;
 
@@ -50,7 +34,7 @@ client.on('message', (message) => {
         case 'ping':
             return message.channel.send('Pong');
         case 'server-info':
-            return message.channel.send(info.serverInfo(message.content));
+            return message.channel.send(info.serverInfo(message));
         case 'icon':
             return message.channel.send(message.author.avatarURL); 
         case 'img':
--- src/commands/server-info.js
+++ src/commands/server-info.js
@@ -1,10 +1,9 @@
+require('discord.js');
+
+function serverInfo(message){
+    return  `**Server Name:** : ${message.guild.name},\n\n**Total Count:** : ${message.guild.memberCount},\n\n**Created** : ${message.guild.createdAt}`;
+}
+
 return module.exports = {
-    serverInfo : message => {
-        return {
-            "**Server Name:**" : message.guild.name,
-            "**Total Count:**" : message.guild.memberCount,
-            "**Region:**" : message.guid.region,
-            "**Created:" : message.guild.createdAt 
-        };
-    }
+    serverInfo :serverInfo
 }
