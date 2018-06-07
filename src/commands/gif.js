809b6cce23e87812e67ef6deaef2eef753030bb9
Preethesh Puthran <puthranpreethesh@gmail.com>
2018-06-07 10:38:27 -0400

    Update gif.js

--- src/commands/gif.js
+++ src/commands/gif.js
@@ -6,7 +6,7 @@ const giphyRandom = new GiphyRandom({ apiKey: key });
 async function getGif(arg) {
     try {
         let result = await giphyRandom.get({tag : arg});
-        let url = await result.images.fixed_height_still.url;
+        let url = result.images.fixed_height_still.url;
     } catch(e) {
         console.log(e);
     }
